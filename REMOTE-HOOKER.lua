75273c98352f8367ec6b962e100521482849abda
NickCoder <16845999+MrNickCoder@users.noreply.github.com>
2026-05-24 21:01:48 +0800

    Update REMOTE-HOOKER.lua

--- REMOTE-HOOKER.lua
+++ REMOTE-HOOKER.lua
@@ -36,9 +36,9 @@ HOOK = hookmetamethod(game, "__namecall", newcclosure(function(...)
                 if not table.find(NPHHOOKWHITELIST, remote.Name) then return HOOK(...); end
 
                 local args = { select(2, ...) };
-                local _, before = pcall(NPHGetData);
+                local before = deepclone(NPHGetData());
                 local returns = { HOOK(...) };
-                local _, after = pcall(NPHGetData);
+                local after = deepclone(NPHGetData());
 
                 local data = {
                     method = method;
