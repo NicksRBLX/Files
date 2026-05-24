c556425cb0ae6ab03075a86d0bb3defa7a778e72
NickCoder <16845999+MrNickCoder@users.noreply.github.com>
2026-05-24 18:40:49 +0800

    Update and rename GTD HOOK.lua to REMOTE-HOOKER.lua

---
+++ REMOTE-HOOKER.lua
@@ -0,0 +1,55 @@
+local cloneref = (cloneref or clonereference or function(instance) return instance; end);
+
+local function deepclone(args: table, copies: table): table
+    local copy = nil
+    copies = copies or {}
+
+    if type(args) == 'table' then
+        if copies[args] then
+            copy = copies[args]
+        else
+            copy = {}
+            copies[args] = copy
+            for i, v in next, args do
+                copy[deepclone(i, copies)] = deepclone(v, copies)
+            end
+        end
+    elseif typeof(args) == "Instance" then
+        copy = cloneref(args)
+    else
+        copy = args
+    end
+    return copy
+end
+
+local HOOK = nil;
+HOOK = hookmetamethod(game, "__namecall", newcclosure(function(...)
+    local method = getnamecallmethod();
+
+    if method and (method == "FireServer" or method == "fireServer" or method == "InvokeServer" or method == "invokeServer") then
+        if typeof(...) == 'Instance' then
+            local remote = cloneref(...);
+
+            if game.IsA(remote, "RemoteEvent") or game.IsA(remote, "RemoteFunction") then
+                if checkcaller() then return HOOK(...); end
+                if not table.find(whitelist, remote.Name) then return HOOK(...); end
+
+                local args = { select(2, ...) };
+                local returns = { HOOK(...) };
+
+                local data = {
+                    method = method,
+                    remote = remote,
+                    args = deepclone(args),
+                    returnvalue = returns,
+                }
+                args = nil
+
+                schedule(remoteHandler, data);
+
+                return unpack(returns);
+            end
+        end
+    end
+    return HOOK(...);
+end));
